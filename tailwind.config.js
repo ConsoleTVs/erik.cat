751e41156f7ff36f87db13cfb3addff98b728187
Erik C. Forés <soc@erik.cat>
2022-03-29 06:51:48 +0200

    remove comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,40 +8,6 @@ module.exports = {
       fontFamily: {
         serif: ['Martel'],
       },
-      // typography: {
-      //   DEFAULT: {
-      //     css: {
-      //       color: colors.black,
-      //       h1: {
-      //         color: colors.black,
-      //         fontWeight: theme.fontWeight.normal,
-      //         fontSize: theme.fontSize['2xl'][0],
-      //         margin: `${theme.spacing['6']} 0`,
-      //         marginTop: `${theme.spacing['12']} !important`,
-      //       },
-      //       h2: {
-      //         color: colors.black,
-      //         fontWeight: theme.fontWeight.normal,
-      //         fontSize: theme.fontSize['xl'][0],
-      //         margin: `${theme.spacing['6']} 0`,
-      //         marginTop: `${theme.spacing['12']} !important`,
-      //       },
-      //       h3: {
-      //         color: colors.black,
-      //         fontWeight: theme.fontWeight.normal,
-      //         fontSize: theme.fontSize['lg'][0],
-      //         margin: `${theme.spacing['6']} 0`,
-      //         marginTop: `${theme.spacing['12']} !important`,
-      //       },
-      //       pre: {
-      //         borderRadius: '0',
-      //         padding: '0',
-      //         color: colors.black,
-      //         background: colors.transparent,
-      //       },
-      //     },
-      //   },
-      // },
     },
   },
   plugins: [require('@tailwindcss/typography')],
