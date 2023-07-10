c87e41529c0d8ad1642e362be2b25a4723990b63
Èrik C. Forés <soc@erik.cat>
2023-07-11 00:13:08 +0200

    feat: add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
+import robots from 'astro-robots-txt'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.MAIN_DOMAIN ?? process.env.CF_PAGES_URL ?? 'https://erik.cat',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap(), robots()],
   markdown: {
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
