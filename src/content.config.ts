ff18fbd5a15e87eb812804536ce4f98309540c40
Erik C. Forés <soc@erik.cat>
2025-01-04 01:40:46 +0100

    fix(core): deprecations on astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,18 @@
-import { defineCollection } from 'astro:content'
+import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({})
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+  }),
+})
 
 export const collections = { blog }
